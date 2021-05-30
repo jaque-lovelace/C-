d1190f0377ca8d29c5af4d66336560efea91079b
Jaqueline Menezes <62194103+jaque-lovelace@users.noreply.github.com>
2021-05-29 23:00:26 -0300

    Add files via upload

---
+++ imc/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome,continua, situacao = "";
+            double peso, altura, imc;
+
+
+
+            do
+            {
+                Console.WriteLine("Entre com o nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Entre com o peso: ");
+                peso = double.Parse(Console.ReadLine());
+                Console.WriteLine("Entre com a altura: ");
+                altura = double.Parse(Console.ReadLine());
+
+                imc = peso / (altura * altura);
+                //Console.WriteLine("O IMC é:" + imc);
+
+
+                if (imc <= 20)
+                {
+                    situacao = " abaixo do peso!";
+                }
+                else
+                {
+                    if (imc > 20 && imc < 25)
+                    {
+                        situacao = " peso normal.";
+                    }
+                    else
+                    {
+                        if (imc >= 25)
+                        {
+                            situacao = " Acima do peso!";
+                        }
+                    }
+                }
+                Console.WriteLine("O IMC é:" + imc + " e você está " + situacao);
+                Console.WriteLine("Quer continuar? (s/n)"); 
+                continua = Console.ReadLine();
+            }while(continua == "s");
+        }
+
+
+
+
+
+
+
+    }
+}
+
---
+++ litrosConsumo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace litrosConsumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distancia, consumo;
+            Console.WriteLine("Digite a distância a ser percorrida em km: ");
+            distancia = float.Parse(Console.ReadLine());
+            consumo = distancia / 12;
+            Console.WriteLine("O consumo em Litros será de: " + consumo + "litros");
+        }
+    }
+}
